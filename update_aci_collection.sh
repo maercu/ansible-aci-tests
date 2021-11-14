890e7c62f24cbb170e9b76be9362d99ea7cf90af
Marcel Zehnder <maercu@macbook.home>
2021-11-14 14:45:48 +0100

    ..

--- update_aci_collection.sh
+++ update_aci_collection.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+echo "Testing $1"
 
 ansible-galaxy collection build --force --output-path /tmp /Users/maercu/dev/ansible-aci
 ansible-galaxy collection install /tmp/cisco-aci-* --force
 
+sleep 10
+
+cd ~/.ansible/collections/ansible_collections/cisco/aci
+ansible-test sanity plugins/modules/$1.py
